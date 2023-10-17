03d480fe930e1886c481d881d460fefad02d90af
pedro-guimel <145490657+pedro-guimel@users.noreply.github.com>
2023-10-17 16:36:19 -0300

    Add files via upload

---
+++ aula01/exp02.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp02</title>
+</head>
+<body>
+    <?php 
+    echo'teste1 <br>';
+    echo'teste2'; echo'<br>';
+    echo'teste3 ' . 'concatenacao' . '<br>';
+    echo 'meu nome é ' . 'maria das onças' . '<br>'
+    
+    ?>
+</body>
+</html>
