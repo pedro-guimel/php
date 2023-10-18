cd8d30771e172a0ef57bc9a065d8161e87d026a8
pedro-guimel <145490657+pedro-guimel@users.noreply.github.com>
2023-10-18 17:28:53 -0300

    Add files via upload

---
+++ aula02/atv01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>atv01</title>
+</head>
+<body>
+
+     <div class="fundo1">
+     <p>"</p>
+
+</div>
+?>
+</body>
+</html>
---
+++ aula02/exp01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp01</title>
+    <link re1="stylesheet" href="exp01.css">
+</head>
+<body>
+    <pre>seletores</pre>
+     <h1>titulo 1 </h1>   
+    <h2>subtitulo</h2>
+    <p>paragrafo</p>
+    <p class="paragrafo2"></p>
+    <p id="paragrafo"></p>
+    <?php
+
+
+?>
+</body>
+</html>
