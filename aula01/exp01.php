0135a88c7f1be4123dfe21b77deb361a3ce3333a
pedro-guimel <145490657+pedro-guimel@users.noreply.github.com>
2023-10-18 14:01:13 -0300

    Add files via upload

--- aula01/exp01.php
+++ aula01/exp01.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exp01</title>
+    <title>exp01 </title>
 </head>
 <body>
-    <?php 
-      echo "php dentro do html";
-    ?>    
-
+    <?php
+    echo "arquivo em php";
+    ?>
+    <sh1> teste de  alteracao de github
+</h1>
 </body>
 </html>
