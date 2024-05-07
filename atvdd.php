d7cc18584cd62fa31a2c10ce407c477cdb39dfa0
pedro-guimel <145490657+pedro-guimel@users.noreply.github.com>
2024-05-07 14:18:56 -0300

    Add files via upload

---
+++ atvdd.php
@@ -0,0 +1,45 @@
+<?php
+
+$aluno["nome"] = "pedro g";
+$aluno["endereco"] = "Rua jose pereira";
+$aluno["cidade"] = "guanambi";
+$aluno["estado"] = "bahia";
+$aluno["idade"] = "17";
+$aluno["cpf"] = "444.777.696-9";
+
+$aluno1["nome"] = "Eric";
+$aluno1["endereco"] = "avenida principal";
+$aluno1["cidade"] = "guanambi";
+$aluno1["estado"] = "bahia";
+$aluno1["idade"] = "19";
+$aluno1["cpf"] = "031.777.000-0";
+
+$aluno2["nome"] = "Lipin";
+$aluno2["endereco"] = "monte azul";
+$aluno2["cidade"] = "guanambi";
+$aluno2["estado"] = "Bahia";
+$aluno2["idade"] = "10";
+$aluno2["cpf"] = "999.999.999-9";
+
+$professor["nome"] = "eber";
+$professor["endereco"] = "Rua esposa";
+$professor["cidade"] = "guanambi";
+$professor["estado"] = "bahia";
+$professor["idade"] = "43";
+$professor["materia"] = "banco de dados";
+
+$professor1["nome"] = "fabio";
+$professor1["endereco"] = "rua html";
+$professor1["cidade"] = "guanambi";
+$professor1["estado"] = "bahia";
+$professor1["idade"] = "150";
+$professor1["materia"] = "psw";
+
+$professor1["nome"] = "joao paulo";
+$professor1["endereco"] = "cabrunco";
+$professor1["cidade"] = "cabeça miuda";
+$professor1["estado"] = "prosaruim";
+$professor1["idade"] = "5";
+$professor1["materia"] = "asw";
+
+?>
